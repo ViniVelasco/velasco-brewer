a4bfca655fe083311678f8ca1b7150645279535b
= <sasukegc@gmail.com>
2018-10-31 19:30:05 -0300

    Upload de foto via ajax no cadastro de cervejas

--- src/main/java/com/velasco/brewer/config/init/AppInitializer.java
+++ src/main/java/com/velasco/brewer/config/init/AppInitializer.java
@@ -3,6 +3,9 @@ package com.velasco.brewer.config.init;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import javax.servlet.Filter;
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletRegistration.Dynamic;
+
 import org.springframework.web.filter.CharacterEncodingFilter;
 
 import com.velasco.brewer.config.JPAConfig;
@@ -33,6 +36,11 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
         characterEncodingFilter.setForceEncoding(true);
 		
         return new Filter[] { characterEncodingFilter };
+	}
+	
+	@Override
+	protected void customizeRegistration(Dynamic registration) {
+		registration.setMultipartConfig(new MultipartConfigElement(""));
 	}
 	
 
