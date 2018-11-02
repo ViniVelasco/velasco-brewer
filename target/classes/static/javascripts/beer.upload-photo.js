eaaf9d64dfe380065018e3b7182eb9b650632b95
= <sasukegc@gmail.com>
2018-11-02 14:20:12 -0300

    Modularizando javascript de adicionar a foto

---
+++ target/classes/static/javascripts/beer.upload-photo.js
@@ -0,0 +1,60 @@
+var Brewer = Brewer || {};
+
+Brewer.UploadPhoto = (function() {
+	
+	function UploadPhoto() {
+		this.inputNamePhoto = $('input[name=photo]');
+		this.inputContentType = $('input[name=contentType]');
+		
+		this.source = $('#beer-photo').html();
+		this.template = Handlebars.compile(this.source);
+		
+		this.containerBeerPhoto = $('.js-container-beer-photo');
+		
+		this.uploadDrop = $('#upload-drop');
+	}
+	
+	UploadPhoto.prototype.initialize  = function () {
+
+		
+		var settings = {
+				type: 'json',
+				filelimit: 1,
+				allow: '*.(jpg|jpeg|png|gif)',
+				action: this.containerBeerPhoto.data('url-photos'),
+				complete: onUploadComplete.bind(this)
+		};
+		
+		UIkit.uploadSelect($('#upload-select'), settings);
+		UIkit.uploadDrop(this.uploadDrop, settings);
+	}
+	
+	function onUploadComplete(response) {
+		this.inputNamePhoto.val(response.name);
+		this.inputContentType.val(response.contentType);
+		
+		this.uploadDrop.addClass('hidden');
+		var html = this.template({photoName: response.name});
+		this.containerBeerPhoto.append(html);
+		
+		$('.js-remove-photo').on('click', onRemovePhoto.bind(this));
+	}
+	
+	function onRemovePhoto() {
+		$('.js-beer-photo').remove();
+		this.uploadDrop.removeClass('hidden');
+		this.inputNamePhoto.val("");
+		this.inputContentType.val("");
+	}
+
+	
+	return UploadPhoto;
+	
+})();
+
+$(function() {
+	
+	var uploadPhoto = new Brewer.UploadPhoto();
+	uploadPhoto.initialize();
+	
+});
