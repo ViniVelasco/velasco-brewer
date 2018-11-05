a99ea786155deb7231f41ef1b72af7753584f851
= <sasukegc@gmail.com>
2018-11-05 20:26:05 -0200

    ordenando pesquisa por sku ou nome

--- src/main/java/com/velasco/brewer/controller/page/PageWrapper.java
+++ src/main/java/com/velasco/brewer/controller/page/PageWrapper.java
@@ -5,6 +5,8 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Order;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -44,5 +46,38 @@ public class PageWrapper<T> {
 	
 	public String urlForPage(int page) {
 		return uriBuilder.replaceQueryParam("page", page).build(true).encode().toUriString();
+	}
+	
+	public String urlOrdered(String property) {
+		UriComponentsBuilder uriBuilderOrder = UriComponentsBuilder.fromUriString(uriBuilder.build(true).encode().toUriString());
+		
+		String sortValue = String.format("%s,%s", property, invertDirection(property));
+		
+		return uriBuilder.replaceQueryParam("sort", sortValue).build(true).encode().toUriString();
+	}
+	
+	public String invertDirection(String property) {
+		String direction = "asc";
+		
+		Order order = page.getSort() != null ? page.getSort().getOrderFor(property) : null;
+		if(order != null) {
+			direction = Sort.Direction.ASC.equals(order.getDirection()) ? "desc" : "asc";
+		}
+		
+		return direction;
+	}
+	
+	public boolean descendent(String property) {
+		return invertDirection(property).equals("asc");
+	}
+	
+	public boolean ordered(String property) {
+		Order order = page.getSort() != null ? page.getSort().getOrderFor(property) : null;
+		
+		if(order == null) {
+			return false;
+		}
+		
+		return page.getSort().getOrderFor(property) != null ? true : false;
 	}
 }
--- src/main/java/com/velasco/brewer/repository/helper/beer/BeersImpl.java
+++ src/main/java/com/velasco/brewer/repository/helper/beer/BeersImpl.java
@@ -6,11 +6,13 @@ import javax.persistence.PersistenceContext;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
@@ -36,6 +38,14 @@ public class BeersImpl implements BeersQueries {
 		
 		criteria.setFirstResult(firstRegister);
 		criteria.setMaxResults(totalRegistersPerPage);
+		
+		Sort sort = pageable.getSort();
+		
+		if (sort != null) {
+			Sort.Order order = sort.iterator().next();
+			String field = order.getProperty();
+			criteria.addOrder(order.isAscending() ? Order.asc(field) : Order.desc(field));
+		}
 		
 		addFilter(filter, criteria);
 		
